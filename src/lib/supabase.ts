14ad56082e3e299eae05ec701f8cb9a6bbe8d19a
ululhikam <cemengsableng@gmail.com>
2026-05-08 02:57:01 +0700

    fix: dotenv lupa di supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,8 @@
 // supabase.ts
 import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL || '';
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.SUPABASE_ANON_KEY || '';
